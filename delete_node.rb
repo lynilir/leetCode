4c5a12afec2a0c3953aa5f5118a19b15dfc0b5bd
Yong Lin <gmale88@gmail.com>
2017-06-27 19:09:25 -0500

    delete node

--- delete_node.rb
+++ delete_node.rb
@@ -1,6 +1,7 @@
 def delete_node(node)
-    next_node = node.next
-    next_next_node = next_node.next
-    node.val = next_node.val
-    node.next = next_next_node
+  #given only access to the node to delete 
+  next_node = node.next
+  next_next_node = next_node.next
+  node.val = next_node.val
+  node.next = next_next_node
 end
